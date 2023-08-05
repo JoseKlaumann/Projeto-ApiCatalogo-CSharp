035f76d553d2fef093fb64f6a03a8f3961935e6b
José Klaumann <jose.augusto.klaumann@hotmail.com>
2023-08-05 19:51:29 -0300

    Implementação da classe controller do produto.

---
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,86 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _contex;
+
+        public ProdutosController(AppDbContext contex)
+        {
+            _contex = contex;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            var produtos = _contex.Produtos.ToList();
+
+            if (produtos is null)
+            {
+                return NotFound("Produtos não encontrados!");
+            }
+
+            return produtos;
+        }
+
+        [HttpGet("{id:int}", Name = "ObterProduto")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _contex.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            if (produto is null)
+            {
+                return NotFound("Produto não encontrado!");
+            }
+            return produto;
+        }
+
+        [HttpPost]
+        public ActionResult Post(Produto produto)
+        {
+            if (produto is null)
+            {
+                return BadRequest();
+            }
+            _contex.Produtos.Add(produto);
+            _contex.SaveChanges();
+            return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Produto produto)
+        {
+            if (id != produto.ProdutoId)
+            {
+                return BadRequest();
+            }
+
+            _contex.Entry(produto).State = EntityState.Modified;
+            _contex.SaveChanges();
+
+            return Ok(produto);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var produto = _contex.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+
+            if (produto is null)
+            {
+                return NotFound("Produto não localizado!");
+            }
+
+            _contex.Produtos.Remove(produto);
+            _contex.SaveChanges();
+
+            return Ok(produto);
+        }
+    }
+}
