4a3a0a1be98c5b47c45ed1129e1cb1dc7bb18114
José Klaumann <jose.augusto.klaumann@hotmail.com>
2023-08-05 19:58:47 -0300

    Implementando a classe controller da categoria.

---
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -0,0 +1,85 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriasController : Controller
+    {
+        private readonly AppDbContext _contex;
+
+        public CategoriasController(AppDbContext contex)
+        {
+            _contex = contex;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Categoria>> Get()
+        {
+            var categorias = _contex.Categorias.ToList();
+
+            if (categorias is null)
+            {
+                return NotFound("categorias não encontrados!");
+            }
+
+            return categorias;
+        }
+
+        [HttpGet("{id:int}", Name = "ObterCategoria")]
+        public ActionResult<Categoria> Get(int id)
+        {
+            var categoria = _contex.Categorias.FirstOrDefault(p => p.CategoriaId == id);
+            if (categoria is null)
+            {
+                return NotFound("Categoria não encontrado!");
+            }
+            return categoria;
+        }
+
+        [HttpPost]
+        public ActionResult Post(Categoria categoria)
+        {
+            if (categoria is null)
+            {
+                return BadRequest();
+            }
+            _contex.Categorias.Add(categoria);
+            _contex.SaveChanges();
+            return new CreatedAtRouteResult("ObterCategoria", new { id = categoria.CategoriaId }, categoria);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Categoria categoria)
+        {
+            if (id != categoria.CategoriaId)
+            {
+                return BadRequest();
+            }
+
+            _contex.Entry(categoria).State = EntityState.Modified;
+            _contex.SaveChanges();
+
+            return Ok(categoria);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var categoria = _contex.Categorias.FirstOrDefault(p => p.CategoriaId == id);
+
+            if (categoria is null)
+            {
+                return NotFound("Categoria não localizado!");
+            }
+
+            _contex.Categorias.Remove(categoria);
+            _contex.SaveChanges();
+
+            return Ok(categoria);
+        }
+    }
+}
